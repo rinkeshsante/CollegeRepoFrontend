493a27c6039509bb5f06d0917ac62ce21a638d09
RinkeshSante <52735633+rinkeshsante@users.noreply.github.com>
2020-09-04 10:24:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { TestDep } from "./components/testDep";
+
+function App() {
+  return (
+    <div className="App">
+      <TestDep></TestDep>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/testDep.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import axios from "axios";
+export class TestDep extends Component {
+  state = {
+    details: [],
+  };
+
+  componentDidMount() {
+    let data;
+
+    axios
+      .get("http://127.0.0.1:8000/departemnts/")
+      .then((res) => {
+        data = res.data;
+        this.setState({
+          details: data,
+        });
+      })
+      .catch((err) => {});
+
+    console.log(data);
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.details.map((detail, id) => (
+            <li>
+              <cite title="Source Title">{detail.name}</cite>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TestDep;
