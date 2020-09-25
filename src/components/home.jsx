e866ce0e45e7314f6b589418658b3f84aeef0c9e
RinkeshSante <rinkoosante@gmail.com>
2020-09-25 15:09:12 +0530

    added modal to home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Modal from "./common/modal";
 
 export class Home extends Component {
   render() {
     return (
       <div>
         <h1>Home</h1>
+        <Modal buttonName="about" modalId="small" header="head">
+          <h1> i am children</h1>
+        </Modal>
       </div>
     );
   }
