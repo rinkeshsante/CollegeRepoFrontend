a704103259981d9ad09d63cb14cebb1b807adc84
RinkeshSante <rinkoosante@gmail.com>
2020-09-26 19:29:05 +0530

    changed layout from container to fluid

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Sidebar from "./components/sidebar";
 
 function App() {
   return (
-    <div className="container">
+    <div className="container-fluid">
       <Navbar></Navbar>
       <div className="row">
         <Sidebar></Sidebar>
