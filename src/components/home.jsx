0433627056b8834e5f5bac513085626d75c7064f
RinkeshSante <rinkoosante@gmail.com>
2020-09-30 14:09:00 +0530

    removed excess import

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,24 +1,17 @@
 import React, { Component } from "react";
-import Modal from "./common/modal";
 import Table from "./common/table";
-import TestDep from "./testDep";
 import { ApiEndpoint } from "../config.json";
-import DetailView from "./common/detailView";
+import MoreOption from "./common/moreOption";
+import MyModal from "./common/myModal";
 
 export class Home extends Component {
   render() {
     return (
       <div>
         <h1>Home</h1>
-        <Table
-          BASE_URL={ApiEndpoint + "departments/"}
-          table_name="Department"
-          cloumn_name={["id", "name"]}
-        ></Table>
-        <DetailView
-          header="hse"
-          data={{ name: "on", age: 90, point: 90.89 }}
-        ></DetailView>
+        <MyModal>
+          <h1>hi</h1>
+        </MyModal>
       </div>
     );
   }
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,7 +3,12 @@ import { Route, Redirect, Switch } from "react-router-dom";
 import ExportCSV from "./exportCsv";
 import Home from "./home";
 import NotFound from "./notFound";
+import ComputerTable from "./tables/computerTable";
 import DepartmentTable from "./tables/departmentTable";
+import EquipmentTable from "./tables/equipmentTable";
+import LabTable from "./tables/labTable";
+import PurchaseTable from "./tables/purchaseTable";
+import SoftwareTable from "./tables/softwareTable";
 
 export class Main extends Component {
   render() {
@@ -11,8 +16,14 @@ export class Main extends Component {
       <div className="col-md-8 order-md-2 ">
         <Switch>
           <Route path="/departments" component={DepartmentTable}></Route>
+          <Route path="/computers" component={ComputerTable}></Route>
+          <Route path="/equipments" component={EquipmentTable}></Route>
+          <Route path="/softwares" component={SoftwareTable}></Route>
+          <Route path="/labs" component={LabTable}></Route>
+          <Route path="/perchases" component={PurchaseTable}></Route>
+
           <Route path="/home" component={Home}></Route>
-          <Route path="/reports" component={ExportCSV}></Route>
+          {/* <Route path="/reports" component={ExportCSV}></Route> */}
           <Route path="/not-found" component={NotFound}></Route>
 
           <Redirect path="/" exact to="/home"></Redirect>
